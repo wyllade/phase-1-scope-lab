44af229410666caadbbf56aba21891516cd7e63d
Emman Oduor <wyllademanu003@gmail.com>
2025-03-07 07:20:21 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+const globalvar = {};
+
+var customerName = 'bob';
+globalvar.customerName = customerName;
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+const leastFavoriteCustomer = 'initialValue';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'newValue'; 
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
